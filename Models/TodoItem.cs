78a998b99b23741b2375ef18c0c933162d87e1ab
matt legesse <mlegesse@gmail.com>
2019-10-01 01:24:24 -0700

    webApi in dotnet 3.0

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace DotNetCoreApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
